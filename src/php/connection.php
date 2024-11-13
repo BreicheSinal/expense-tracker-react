3a93d33803de820fe575cbd59e72728267a83e97
BreicheSinal <sinalbreiche@gmail.com>
2024-11-13 20:52:47 +0200

    Added headers in connection php file

--- src/php/connection.php
+++ src/php/connection.php
@@ -1,6 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000"); 
+header("Access-Control-Allow-Methods: DELETE, OPTIONS"); 
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
