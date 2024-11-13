6efd4e892f428ae851db5964a7434118d7e74c26
BreicheSinal <sinalbreiche@gmail.com>
2024-11-13 02:55:44 +0200

    Finished Login Form JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import LoginForm from "./components/LoginForm";
+
 const App = () => {
   console.log("App rerendered");
 
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default App;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 
+import "../styles/base.css";
+import "../styles/utilities.css";
+import "../styles/colors.css";
+import "../styles/fonts.css";
+import "../styles/LoginForm.css";
+
+import logo from "../assets/icons/LOGO.png";
+
 const LoginForm = () => {
   return (
     <div>
-      <div
-        id="errorMsg"
-        class="center errorMsg text-align bold primary-bgColor white-color"
-        style="display: none"
-      ></div>
-      <div class="flex column center login white-bgColor">
+      <div className="flex column center login white-bgColor">
         <img
-          src="assets/icons/LOGO.png"
+          src={logo}
           alt="LOGO"
           width="100px"
           height="100px"
-          class="align-self"
+          className="align-self"
         />
 
-        <h2 class="signUpt text-align primary-color">SIGN UP</h2>
-        <h2 class="logint text-align primary-color">LOGIN</h2>
+        <h2 className="signUpt text-align primary-color">SIGN UP</h2>
+        <h2 className="logint text-align primary-color">LOGIN</h2>
 
-        <div class="flex column gap primary-color">
-          <div class="flex column gap primary-color">
+        <div className="flex column gap primary-color">
+          <div className="flex column gap primary-color">
             <label for="username">Username</label>
             <input type="text" placeholder="Username" id="username" required />
           </div>
 
-          <div class="flex column gap primary-color">
+          <div className="flex column gap primary-color">
             <label for="password">Password</label>
             <input
               type="password"
@@ -37,20 +40,20 @@ const LoginForm = () => {
           </div>
 
           <div>
-            <hr style="margin-top: 10px" class="primary-bgColor" />
+            <hr className="primary-bgColor" />
           </div>
 
           <button
             type="submit"
-            class="register button white-color"
+            className="register button white-color"
             id="loginBttn"
           >
             LOG IN
           </button>
-          <p class="text-align">OR</p>
+          <p className="text-align">OR</p>
           <button
             type="submit"
-            class="register button white-color"
+            className="register button white-color"
             id="signUpBttn"
           >
             SIGN UP
